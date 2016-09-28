31a460219a295a7db9aaacf22c84449fd414674c
Alexander Kondratenko <alexk.wellbe@gmail.com>
2016-09-28 22:05:03 +0300

    Add some packages, add bower_components autoload and build from index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,34 +23,103 @@ module.exports = function(grunt) {
         // Automatically inject Bower components into the app
         wiredep: {
             app: {
-                src: 'app/index222.html'
+                src: ['<%= gProj.app %>/index.html'],
+                exclude: [
+                    '/wow/',
+                    '/cropperjs/'
+                ],
+                ignorePath:  /\.\.\//
+            }
+        },
+
+        // Empties folders to start fresh
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%= gProj.dist %>/{,*/}*',
+                        '!<%= gProj.dist %>/.git{,*/}*'
+                    ]
+                }]
             }
         },
 
         concat: {
-            libs: {
-                files: {
-                    'dist/js/vendor.js': [ 'bower_components/angular/angular.js', 'bower_components/cropperjs/dist/cropper.js' ],
-                    'dist/js/app.js': [ 'app/js/*.js' ]
-                }
+            scripts: {
+                src: '<%= gProj.app %>/js/*.js',
+                dest: '<%= gProj.dist %>/js/app.js'
             },
             styles: {
-                files: {
-                    'dist/css/vendor.css': [ 'bower_components/bootstrap-css-only/css/bootstrap.css', 'bower_components/cropperjs/dist/cropper.css' ],
-                    'dist/css/styles.css': [ 'app/css/*.css' ]
-                }
+                src: '<%= gProj.app %>/css/*.css',
+                dest: '<%= gProj.dist %>/css/styles.css'
             }
         },
+
         uglify: {
             dist: {
                 files: {
-                    'dist/js/vendor.js': [ 'dist/js/vendor.js' ],
-                    'dist/js/app.js': [ 'dist/js/app.js' ],
+                    '<%= gProj.dist %>/js/vendor.js': [ '<%= gProj.dist %>/js/vendor.js' ],
+                    '<%= gProj.dist %>/js/app.js': [ '<%= gProj.dist %>/js/app.js' ],
                 }
             }
+        },
+
+
+        // Renames files for browser caching purposes
+        filerev: {
+            dist: {
+                src: [
+                  '<%= gProj.dist %>/js/{,*/}*.js',
+                  '<%= gProj.dist %>/css/{,*/}*.css',
+                  '<%= gProj.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+                  '<%= gProj.dist %>/css/fonts/*'
+                ]
+            }
+        },
+
+        // Reads HTML for usemin blocks to enable smart builds that automatically
+        // concat, minify and revision files. Creates configurations in memory so
+        // additional tasks can operate on them
+        useminPrepare: {
+            html: '<%= gProj.app %>/index.html',
+            options: {
+                dest: '<%= gProj.dist %>',
+                flow: {
+                    html: {
+                        steps: {
+                            js: ['concat', 'uglify']
+                            //css: ['cssmin']
+                        }
+                    }
+                }
+            }
+        },
+
+        // Performs rewrites based on filerev and the useminPrepare configuration
+        usemin: {
+            html: ['<%= gProj.dist %>/{,*/}*.html'],
+            css: ['<%= gProj.dist %>/css/{,*/}*.css'],
+            options: {
+                assetsDirs: [
+                  '<%= gProj.dist %>',
+                  '<%= gProj.dist %>/images',
+                  '<%= gProj.dist %>/css'
+                ]
+            }
         }
+
     });
 
-    grunt.registerTask('default', ['concat', 'uglify']);
+    grunt.registerTask('default', [
+        'clean:dist',
+        'wiredep',
+        'useminPrepare',
+        'concat',
+        'uglify',
+        'filerev',
+        'usemin',
+    ]);
 
 };
